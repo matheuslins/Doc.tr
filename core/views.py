17e818d789481f88f21df2af1f361a8848a58ccc
Matheus Lins <msl@cin.ufpe.br>
2015-11-04 12:18:34 -0300

    colocando o front da parte de cadastro

--- core/views.py
+++ core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def home(request):
 	template_name = 'index.html'
 	return render(request, template_name)
+
+def register(request):
+	template_name = 'register.html'
+	return render(request, template_name)
